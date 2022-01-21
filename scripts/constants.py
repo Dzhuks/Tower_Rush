c361909969789accd6beb2d20617c83cf550caf3
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-21 20:32:20 +0300

    Add constants.py

---
+++ scripts/constants.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+SIZE = WIDTH, HEIGHT = 600, 400
+FPS = 60
+ALL_SPRITES = pygame.sprite.Group()
+ENEMIES_SPRITES = pygame.sprite.Group()
+PLAYER_SPRITES = pygame.sprite.Group()
+TOWER_SPRITES = pygame.sprite.Group()
