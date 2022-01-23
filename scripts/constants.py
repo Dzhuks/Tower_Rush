d4380a2e7e29b8b992f0ab26f0467aa376db052e
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-23 20:35:39 +0300

    Add load_sound and load_sound functions

--- scripts/constants.py
+++ scripts/constants.py
@@ -13,7 +13,7 @@ def load_image(name, colorkey=None):
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        terminate()
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -23,6 +23,22 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def load_sound(name):
+    fullname = os.path.join('data', 'sounds', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл со звуком '{fullname}' не найден")
+        terminate()
+    return pygame.mixer.Sound(fullname)
+
+
+def load_sound(name, size=30):
+    fullname = os.path.join('data', 'fonts', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с шрифтом '{fullname}' не найден")
+        terminate()
+    return pygame.font.Font(fullname, size)
 
 
 SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 400
