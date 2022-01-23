f0eb96fee36cefd15ab006b50ce5f3f13ce533bf
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-23 18:48:09 +0300

    constants and functions moved to a separate file

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,7 +1,32 @@
 import pygame
+import sys
+import os
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 SIZE = WIDTH, HEIGHT = 600, 400
+SCREEN = pygame.display.set_mode(SIZE)
 FPS = 60
 ALL_SPRITES = pygame.sprite.Group()
 ENEMIES_SPRITES = pygame.sprite.Group()
