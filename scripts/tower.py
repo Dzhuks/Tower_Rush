459c3248475673543db04d4e5b8f39648b26b279
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-24 22:56:31 +0300

    tower.py finished

--- scripts/tower.py
+++ scripts/tower.py
@@ -1,10 +1,9 @@
-import pygame
 from units import *
 
 
 class Tower(pygame.sprite.Sprite):
-    def __init__(self, whole_img, broken_img, hp, x, y):
-        super(Tower, self).__init__()
+    def __init__(self, whole_img, broken_img, hp, x, y, *groups):
+        super(Tower, self).__init__(*groups)
         self.whole_img = whole_img
         self.broken_img = broken_img
         self.image = self.whole_img
@@ -12,30 +11,34 @@ class Tower(pygame.sprite.Sprite):
         self.cur_hp = hp
         self.rect = self.image.get_rect()
         self.rect.move(x, y)
-        self.is_alive = True
+        self.is_whole = True
 
-    def attacked(self, damage):
+    def defense(self, damage):
         self.cur_hp -= damage
         if self.cur_hp <= 0:
-            self.is_alive = False
+            self.is_whole = False
+
+    def draw_health_bar(self, win):
+        font = pygame.font.Font(None, 30)
+        string_rendered = font.render(f"{self.cur_hp}/{self.max_hp}", True, ORANGE)
+        win.blit(string_rendered, (win.get_width() - string_rendered.get_width(), 0))
+
+    def is_broken(self):
+        return self.is_whole
 
     def update(self):
-        if self.is_alive:
+        if self.is_whole:
             self.image = self.whole_img
         else:
             self.image = self.broken_img
-        font = pygame.font.Font(None, 30)
-        string_rendered = font.render(f"{self.cur_hp}/{self.max_hp}", True, (255, 165, 0))
-        self.image.blit(string_rendered, (self.image.get_width() - string_rendered.get_width(), 0))
+        self.draw_health_bar(self.image)
 
 
 class EnemyTower(Tower):
     def spawn(self, name):
-        unit = EnemyUnit(name, self.rect.x + self.rect.width / 2, self.rect.y)
-        ENEMIES_SPRITES.add(unit)
+        EnemyUnit(name, self.rect.x + self.rect.width / 2, self.rect.y, ENEMIES_SPRITES)
 
 
 class PlayerTower(Tower):
     def spawn(self, name):
-        unit = PlayerUnit(name, self.rect.x + self.rect.width / 2, self.rect.y)
-        PLAYER_SPRITES.add(unit)
+        Unit(name, self.rect.x + self.rect.width / 2, self.rect.y, PLAYER_SPRITES)
