71140b56ae0b4761a31dfeee9fd97e575570e7d8
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-24 22:58:49 +0300

    menu.py finished

--- scripts/menu.py
+++ scripts/menu.py
@@ -3,8 +3,8 @@ from scripts.game import Game
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, name, img, x, y):
-        super(Button, self).__init__()
+    def __init__(self, name, img, x, y, *group):
+        super(Button, self).__init__(*group)
         self.name = name
         self.image = img
         self.rect = self.image.get_rect()
@@ -16,19 +16,21 @@ class Button(pygame.sprite.Sprite):
 
 
 class BuyButton(Button):
-    def __init__(self, unit_name, cost, img, x, y):
-        super(BuyButton, self).__init__(unit_name, img, x, y)
+    def __init__(self, unit_name, cost, img, x, y, *group):
+        super(BuyButton, self).__init__(unit_name, img, x, y, *group)
         self.cost = cost
         font = pygame.font.Font(None, 20)
         cost_text = font.render(str(self.cost), True, ORANGE)
         cost_text_x = self.image.get_width() - cost_text.get_width()
         cost_text_y = self.image.get_height() - cost_text.get_height()
         self.image.blit(cost_text, (cost_text_x, cost_text_y))
+        rect = (0, 0, self.image.get_width(), self.image.get_height())
+        pygame.draw.rect(self.image, BLACK, rect, 1)
 
 
 class PauseButton(Button):
-    def __init__(self, name, pause_img, active_img, x, y):
-        super(PauseButton, self).__init__(name, pause_img, x, y)
+    def __init__(self, name, pause_img, active_img, x, y, *group):
+        super(PauseButton, self).__init__(name, pause_img, x, y, *group)
         self.pause_img = pause_img
         self.active_img = active_img
         self.pause = False
@@ -49,21 +51,22 @@ class Menu:
     def draw(self, window: pygame.Surface):
         image = self.bg.copy()
         self.buttons.draw(image)
-        window.blit(image, *self.rect.topleft)
+        window.blit(image, self.rect.topleft)
 
 
 class BuyMenu(Menu):
-    def add_button(self, unit_name, cost, btn_img: pygame.Surface):
-        btn_width, btn_height = 110, 60
-        btn_img = pygame.transform.scale(btn_img, (btn_width, btn_height))
+    def add_button(self, unit_name, cost, unit_img: pygame.Surface):
+        btn_width, btn_height = 75, 50
+        rect = (unit_img.get_width() / 2 - btn_width / 2, unit_img.get_height() / 2 - btn_height / 2, btn_width, btn_height)
+        btn_img = unit_img.subsurface(rect)
         btn_x = self.rect.x + (btn_width + 10) * len(self.buttons.sprites())
         btn_y = self.rect.y + self.rect.height - btn_height
-        self.buttons.add(BuyButton(unit_name, cost, btn_img, btn_x, btn_y))
+        self.buttons.add(BuyButton(unit_name, cost, btn_img, btn_x, btn_y, self.buttons))
 
     def get_clicked(self, pos):
         for btn in self.buttons.sprites():
             if btn.is_clicked(pos):
-                return btn.name
+                return btn.name, btn.cost
 
         return None
 
@@ -99,7 +102,6 @@ class MainMenu(Menu):
                         game = Game()
                         game.run(win)
                         running = False
-                        print('kdjifjkfk')
 
             win.fill(pygame.color.Color("black"))
             win.blit(self.bg, self.rect.topleft)
