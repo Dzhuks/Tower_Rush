d83c900dba1a864822c134ff40ea65f487527c1f
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-26 10:37:57 +0300

    delete attack animation add tba

--- scripts/units.py
+++ scripts/units.py
@@ -1,7 +1,40 @@
 import sqlite3
+import random
 
 import pygame.sprite
 from scripts.constants import *
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(ALL_SPRITES)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if self.rect.width > SCREEN_WIDTH or self.rect.height > SCREEN_HEIGHT:
+            self.kill()
+            del self
 
 
 def hitbox_collision(sprite1, sprite2):
@@ -35,6 +68,7 @@ class Unit(pygame.sprite.Sprite):
         self.damage = cur.execute(f"SELECT damage FROM units WHERE name={name}").fetchall()[0][0]
         speed = cur.execute(f"SELECT speed FROM units WHERE name={name}").fetchall()[0][0]
         self.time_for_move = FPS // speed
+        self.tba = cur.execute(f"SELECT TBA FROM units WHERE name={name}").fetchall()[0][0]
         self.speed = 1
         self.cost = cur.execute(f"SELECT cost FROM units WHERE name={name}").fetchall()[0][0]
         self.money = cur.execute(f"SELECT money FROM units WHERE name={name}").fetchall()[0][0]
@@ -43,11 +77,6 @@ class Unit(pygame.sprite.Sprite):
         moving_animation = load_image(moving_animation)
         self.cur_moving_frame = -1
         self.move_frames = self.cut_sheet(moving_animation)
-
-        attack_animation = cur.execute(f"SELECT attack_animation FROM units WHERE name={name}").fetchall()[0][0]
-        attack_animation = load_image(attack_animation)
-        self.cur_attacking_frame = -1
-        self.attack_frames = self.cut_sheet(attack_animation)
 
         death_animation = cur.execute(f"SELECT death_animation FROM units WHERE name={name}").fetchall()[0][0]
         death_animation = load_image(death_animation)
@@ -76,25 +105,35 @@ class Unit(pygame.sprite.Sprite):
             self.image = self.death_frames[self.cur_death_frame]
 
     def move(self):
-        self.cur_attacking_frame = 0
+        cur = self.con.cursor()
+        self.tba = cur.execute(f"SELECT TBA FROM units WHERE name={self.name}").fetchall()[0][0]
         if self.iteration % self.time_for_move:
             self.rect.x += self.speed
             self.cur_moving_frame = (self.cur_moving_frame + 1) % len(self.move_frames)
             self.image = self.move_frames[self.cur_moving_frame]
 
     def attack(self, group: list):
+        cur = self.con.cursor()
         self.cur_moving_frame = 0
-        self.cur_attacking_frame = self.cur_attacking_frame + 1
-        self.image = self.attack_frames[self.cur_attacking_frame]
-        if self.cur_attacking_frame == len(self.attack_frames) - 1:
-            self.cur_attacking_frame = 0
+        self.tba -= 1
+        if self.tba == 0:
             for unit in group:
                 unit.defense(self.damage)
+            self.tba = cur.execute(f"SELECT TBA FROM units WHERE name={self.name}").fetchall()[0][0]
+
+    def create_particles(self, position):
+        # количество создаваемых частиц
+        particle_count = 20
+        # возможные скорости
+        numbers = range(-5, 6)
+        for _ in range(particle_count):
+            Particle(position, random.choice(numbers), random.choice(numbers))
 
     def defense(self, damage):
         self.hp -= damage
         if self.hp <= 0:
             self.alive = False
+        self.create_particles(self.rect.center)
 
     def get_money(self):
         if self.alive:
