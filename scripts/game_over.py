345430fc378cfe42039b7ff84ad4b9cde66cf132
Seidazymov Adil <m.kypshakb@gmail.com>
2022-01-27 23:50:45 +0300

    Сделал так чтобы постепенно картинка стала более реальной

--- scripts/game_over.py
+++ scripts/game_over.py
@@ -1,13 +1,14 @@
-import pygame
 from scripts.constants import *
 
 
 def game_over(win: pygame.Surface):
+    alpha = 0
     fon = load_image("game_over\\game_over.png")
     font = load_font("coldnightforalligators.otf", 60)
     string_rendered = font.render("Переиграл   и   Уничтожил", True, BLOAD_RED)
     text_coord = win.get_width() / 2 - string_rendered.get_width() / 2, 265
     string_rendered = pygame.transform.scale(string_rendered, (string_rendered.get_width(), 100))
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,7 +16,12 @@ def game_over(win: pygame.Surface):
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 terminate()
                 return  # начинаем игру
+
         win.fill(BLACK)
+        fon.set_alpha(alpha)
+        string_rendered.set_alpha(alpha)
         win.blit(fon, (0, 0))
         win.blit(string_rendered, text_coord)
         pygame.display.flip()
+        alpha += 1
+        clock.tick(FPS)
